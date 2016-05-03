616edc895a88636d2e322d263b2bc9ce005622ae
Nathan Reinauer <nathanreinauer@gmail.com>
2016-05-03 16:50:09 -0700

    Wrote some sick code.

--- Program1/Program1/Program.cs
+++ Program1/Program1/Program.cs
@@ -1,15 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Program1
 {
-    class Program
+    public class Program
     {
+        // This is where your program starts
         static void Main(string[] args)
         {
+            // Prompt user to enter a name.
+            Console.WriteLine("Enter your name, please:");
+
+            // Now read the name entered.
+            string name = Console.ReadLine();
+
+            // Greet the user with the name that was entered.
+            Console.WriteLine("Hello, " + name);
+
+            // Wait for user to acknowledge the results.
+            Console.WriteLine("Press Enter to terminate...");
+
         }
     }
 }
