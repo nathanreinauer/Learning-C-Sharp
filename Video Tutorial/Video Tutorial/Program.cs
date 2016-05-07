69ade7aaac5fe7cc8035b5938f93be55ff3ac84c
Nathan Reinauer <nathanreinauer@gmail.com>
2016-05-06 17:41:51 -0700

    About 5 minutes into tutorial

--- Video Tutorial/Video Tutorial/Program.cs
+++ Video Tutorial/Video Tutorial/Program.cs
@@ -10,6 +10,20 @@ namespace Video_Tutorial
     {
         static void Main(string[] args)
         {
+            bool canVote = true;
+
+            char grade = 'A';
+
+            int maxInt = int.MaxValue;
+            long maxLong = long.MaxValue;
+            decimal maxDec = decimal.MaxValue;
+            float maxFloat = float.MaxValue;
+            double maxDouble = double.MaxValue;
+
+            Console.WriteLine("Max Int: " + maxDouble);
+            var anotherName = "Tom";
+
+
         }
     }
 }
