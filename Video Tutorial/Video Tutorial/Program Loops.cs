76eb674a25abfa0e37cec752faab407d152e029f
Nathan Reinauer <nathanreinauer@gmail.com>
2016-05-16 20:13:54 -0700

    More enum

--- Video Tutorial/Video Tutorial/Program Loops.cs
+++ Video Tutorial/Video Tutorial/Program Loops.cs
@@ -21,5 +21,27 @@ namespace Video_Tutorial
         static void Main(string[] args)
         {
 
-        }
+        Temperature micTemp = Temperature.Low;
+
+        switch (micTemp)
+        {
+            case Temperature.Freeze:
+                Console.WriteLine("Temp on Freezing");
+                break;
+
+            case Temperature.Low:
+                Console.WriteLine("Temp on Low");
+                break;
+
+            case Temperature.Warm:
+                Console.WriteLine("Temp on Warm");
+                break;
+
+            case Temperature.Boil:
+                Console.WriteLine("Temp on Boil");
+                break;
+
+        }    
+
+    }
 }
