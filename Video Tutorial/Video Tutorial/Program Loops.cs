7272f2a2eab035412815c254d44cd1be5871679a
Nathan Reinauer <nathanreinauer@gmail.com>
2016-05-16 21:15:22 -0700

    Done with ENUM

--- Video Tutorial/Video Tutorial/Program Loops.cs
+++ Video Tutorial/Video Tutorial/Program Loops.cs
@@ -16,32 +16,32 @@ namespace Video_Tutorial
     }
 
     class Animal
-    { }
+    {
 
         static void Main(string[] args)
         {
 
-        Temperature micTemp = Temperature.Low;
+            Temperature micTemp = Temperature.Boil;
 
-        switch (micTemp)
-        {
-            case Temperature.Freeze:
-                Console.WriteLine("Temp on Freezing");
-                break;
+            switch (micTemp)
+            {
+                case Temperature.Freeze:
+                    Console.WriteLine("Temp on Freezing");
+                    break;
 
-            case Temperature.Low:
-                Console.WriteLine("Temp on Low");
-                break;
+                case Temperature.Low:
+                    Console.WriteLine("Temp on Low");
+                    break;
 
-            case Temperature.Warm:
-                Console.WriteLine("Temp on Warm");
-                break;
+                case Temperature.Warm:
+                    Console.WriteLine("Temp on Warm");
+                    break;
 
-            case Temperature.Boil:
-                Console.WriteLine("Temp on Boil");
-                break;
+                case Temperature.Boil:
+                    Console.WriteLine("Temp on Boil");
+                    break;
 
-        }    
-
+            }
+        }
     }
 }
