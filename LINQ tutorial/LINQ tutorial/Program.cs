21253200470f54d1b331a9526c38095e3ea579f1
Nathan Reinauer <nathanreinauer@gmail.com>
2016-05-17 21:47:13 -0700

    New video tutorial on LINQ

---
+++ LINQ tutorial/LINQ tutorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sample = "I enjoy writing uber-software in C#";
+
+            var results = from c in sample
+                          select c;
+
+            foreach (var item in results)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
