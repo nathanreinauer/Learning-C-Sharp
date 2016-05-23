e9ee3d1793c7899dac288c3fa0bc6ebd244780ee
Nathan Reinauer <nathanreinauer@gmail.com>
2016-05-23 16:27:54 -0700

    Overloading Drill (two of the same method with different parameters)

---
+++ Csharp Drill 1/Csharp Drill 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Drill_1
+{
+    public class Example
+    {
+        public int add(int first, int second, int third)
+        {
+            return first + second + third;
+        }
+        public int add(int first, int second)
+        {
+            return first + second;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Example adding = new Example();
+            Console.WriteLine(adding.add(1,1,1));
+            Console.WriteLine(adding.add(1,2));
+        }
+    }
+}
