151e326d27b2368b9a1d4d2c9e3b35c85af23b85
Nathan Reinauer <nathanreinauer@gmail.com>
2016-05-28 21:33:33 -0700

    Robert Tabor tutorial

---
+++ Drill3/Drill3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drill3
+{
+    sealed class Nope
+    {
+        int one = 1;
+        int two = 2;
+    }
+    class A : Nope
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wha");
+        }
+    }
+}
