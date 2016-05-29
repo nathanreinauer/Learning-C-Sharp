c2862635348cbba48985f6c5e1f4aa2290906936
Nathan Reinauer <nathanreinauer@gmail.com>
2016-05-28 22:44:01 -0700

    For Iteration (for loop) and some debugging junk

--- ForIteration/ForIteration/Program.cs
+++ ForIteration/ForIteration/Program.cs
@@ -10,6 +10,24 @@ namespace ForIteration
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i < 10; i++)
+            {
+                //Console.WriteLine(i);
+                if (i == 7)
+                {
+                    Console.WriteLine("Found seven!");
+                    break;
+                }
+            }
+
+
+            for (int myValue = 0; myValue < 12; myValue++)
+            {
+                Console.WriteLine(myValue);
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
