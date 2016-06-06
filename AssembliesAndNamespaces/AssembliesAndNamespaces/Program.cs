3b897c413ca3c84d0e981a9a1e6c19a21309a3aa
Nathan Reinauer <nathanreinauer@gmail.com>
2016-06-06 16:08:34 -0700

    Robert Tabor: Learning about Name Spaces and how they relate to classes. Wrote some text to a textfile as an example.

---
+++ AssembliesAndNamespaces/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AssembliesAndNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = "We want to write this to our file.";
+
+            File.WriteAllText(@"C:\Users\Student\Desktop\WriteText.txt", text);
+            Console.WriteLine("Hello world!");
+            Console.ReadLine();
+        }
+    }
+}
