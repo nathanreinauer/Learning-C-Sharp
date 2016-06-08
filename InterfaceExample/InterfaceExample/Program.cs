ee24de0dfb4a14c9b53d336352748550af2b8e1d
Nathan Reinauer <nathanreinauer@gmail.com>
2016-06-08 16:38:21 -0700

    C# Drill - Interface example

---
+++ InterfaceExample/InterfaceExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExample
+{
+    interface IHello
+    {
+        string printHello();
+    }
+    class Hello : IHello
+    {
+        public string printHello()
+        {
+            Console.WriteLine("Hello!");
+            return null;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hello hi = new Hello();
+            hi.printHello();
+        }
+    }
+}
