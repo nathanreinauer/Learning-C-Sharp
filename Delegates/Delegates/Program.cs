cda625757ea2ab03f4dd4ad93d18e1a473e16b8f
Nathan Reinauer <nathanreinauer@gmail.com>
2016-06-08 19:31:52 -0700

    Delegates

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Program
+    {
+        public delegate void Del(string message);
+
+        public static void DelMethod(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static void Main(string[] args)
+        {
+            Del helloMessage = DelMethod;
+            helloMessage("Hello!");
+
+        }
+    }
+}
