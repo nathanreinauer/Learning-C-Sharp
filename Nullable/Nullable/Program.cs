4be97dd6e99cca17d7c0e257bfb03fbd9d896956
Nathan Reinauer <nathanreinauer@gmail.com>
2016-06-08 20:02:53 -0700

    Nullable

---
+++ Nullable/Nullable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? x = 7;
+
+            if (x.HasValue)
+            {
+                Console.WriteLine("x = " + x.Value);
+            }
+            else
+            {
+                Console.WriteLine("x = Null");
+            }
+        }
+    }
+}
