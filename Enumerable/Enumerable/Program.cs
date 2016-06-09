2684ff52c7f2da474706e6052f91c8bf9f7e0e30
Nathan Reinauer <nathanreinauer@gmail.com>
2016-06-08 20:17:52 -0700

    Enumerable

---
+++ Enumerable/Enumerable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumerableExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> result = from value in Enumerable.Range(0, 10)
+                                      select value;
+
+            foreach (int value in result)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
