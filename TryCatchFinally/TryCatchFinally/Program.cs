e7cb02d91dc2d7504f507c931946ab5ac5bb2630
Nathan Reinauer <nathanreinauer@gmail.com>
2016-06-08 17:17:22 -0700

    C# Drill - Writing error to logfile

---
+++ TryCatchFinally/TryCatchFinally/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchFinally
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string path = @"C:\Users\Student\Desktop\text.txt";
+                File.ReadAllLines(path);
+            }
+            catch (Exception e)
+            {
+                string path = @"C:\Users\Student\Desktop\logfile.txt";
+                File.WriteAllText(path, e.Message);
+                Console.WriteLine("Error written to logfile.");
+            }
+            finally
+            {
+                Console.WriteLine("Finished.");
+            }
+
+        }
+    }
+}
