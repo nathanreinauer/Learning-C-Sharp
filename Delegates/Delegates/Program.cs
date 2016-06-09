c92ea43891668bb1c64c4b2c6fe7efa7b7c5f043
Nathan Reinauer <nathanreinauer@gmail.com>
2016-06-08 19:45:26 -0700

    Multicast Delegate

--- Delegates/Delegates/Program.cs
+++ Delegates/Delegates/Program.cs
@@ -15,11 +15,22 @@ namespace Delegates
             Console.WriteLine(message);
         }
 
+        public static void DelMethod2(string message2)
+        {
+            Console.WriteLine(message2);
+        }
+
+
+
         static void Main(string[] args)
         {
-            Del helloMessage = DelMethod;
-            helloMessage("Hello!");
+            Del thing1, thing2, multi;
+            thing1 = DelMethod;
+            thing2 = DelMethod2;
 
+            multi = thing1 + thing2;
+
+            multi("Twice");
         }
     }
 }
