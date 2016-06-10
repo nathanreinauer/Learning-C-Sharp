a8666d923abf64111737daaacd0140da6c4d8637
Nathan Reinauer <nathanreinauer@gmail.com>
2016-06-09 19:22:13 -0700

    String VS StringBuilder

---
+++ BlobWrite/BlobWrite/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace BlobWrite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Turn an image into a byte array
+            string path = @"C:\Users\Student\Desktop\image.jpg";
+            FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+            BinaryReader reader = new BinaryReader(fs);
+            byte[] BlobValue = reader.ReadBytes((int)fs.Length);
+            fs.Close();
+            reader.Close();
+
+            // Write the byte array to a text file
+            FileStream fs2 = new FileStream(@"C:\Users\Student\Desktop\Blob.txt", FileMode.Create);
+            BinaryFormatter serializer = new BinaryFormatter();
+            serializer.Serialize(fs2, BlobValue);
+            fs2.Close();
+            
+            // Deserialize back into byte array
+            byte[] serializedData = null;
+            fs = new FileStream(@"C:\Users\Student\Desktop\Blob.txt", FileMode.Open);
+            serializedData = (byte[])serializer.Deserialize(fs);
+            fs.Close();
+
+            Console.WriteLine("Done.");
+        }
+    }
+}
---
+++ StringBuilderVSString/StringBuilderVSString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuilderVSString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // A string is immutable. It represents one memory location and can't change.
+            string myString = "Goodbye!";
+            Console.WriteLine(myString);
+
+            // A StringBuilder is mutable and can be changed and added to.
+            StringBuilder myStringBuilder = new StringBuilder("Hello! ");
+            myStringBuilder.Append(myString);
+            Console.WriteLine(myStringBuilder);
+
+        }
+    }
+}
