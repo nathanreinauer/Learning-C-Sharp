70c83f467b24faa21fd4872a557cd8750f6e101a
Nathan Reinauer <nathanreinauer@gmail.com>
2016-06-12 16:47:48 -0700

    Operators 2

--- Drillz/Drillz/Program.cs
+++ Drillz/Drillz/Program.cs
@@ -11,15 +11,17 @@ namespace Drillz
     {
         static void Main(string[] args)
         {
-            int x = 10;
             int y = 5;
-            Console.WriteLine("x = {0}", x = y + 3);
-            Console.WriteLine("x = {0}", x = y - 2);
-            Console.WriteLine("x = {0}", x = y * 5);
+            int x = 10;
+            Console.WriteLine("x = {0}", x += y);
             x = 10;
-            Console.WriteLine("x = {0}", x = x / y);
+            Console.WriteLine("x = {0}", x -= y - 2);
             x = 10;
-            Console.WriteLine("x = {0}", x = x % y);
+            Console.WriteLine("x = {0}", x *= y * 5);
+            x = 10;
+            Console.WriteLine("x = {0}", x /= x / y);
+            x = 10;
+            Console.WriteLine("x = {0}", x %= y);
 
         }
     }
