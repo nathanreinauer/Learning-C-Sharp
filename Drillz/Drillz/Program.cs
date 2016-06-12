cd1b0e620912647fa186708688305eaf8f9e68db
Nathan Reinauer <nathanreinauer@gmail.com>
2016-06-12 15:08:39 -0700

    Exercise #4

--- Drillz/Drillz/Program.cs
+++ Drillz/Drillz/Program.cs
@@ -10,14 +10,9 @@ namespace Drillz
     {
         static void Main(string[] args)
         {
-            int num1 = 10;
-            float num2 = 12.5f;
-            string msg = "C# Programming";
-
-            Console.WriteLine(num1);
-            Console.WriteLine(num2);
-            Console.WriteLine(msg);
-
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, {0}!", name);
 
         }
     }
