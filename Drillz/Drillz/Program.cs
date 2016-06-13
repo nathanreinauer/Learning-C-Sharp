7d3690e04e46e4f644ea3bed2a5c48a181176898
Nathan Reinauer <nathanreinauer@gmail.com>
2016-06-12 17:57:25 -0700

    for loop 1

--- Drillz/Drillz/Program.cs
+++ Drillz/Drillz/Program.cs
@@ -11,22 +11,12 @@ namespace Drillz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is the correct way to declare a variable to store an integer value in C#?");
-            Console.WriteLine("a. int 1x=10;");
-            Console.WriteLine("b. int x=10;");
-            Console.WriteLine("c. float x=10.0f;");
-            Console.WriteLine("d. string x=\"10\";");
-
-            var answer = Console.ReadLine();
-            
-            switch (answer)
+            int i, j;
+            for (i = 0; i <= 6; i++)
             {
-                case "a": Console.WriteLine("Incorrect."); break;
-                case "b": Console.WriteLine("Correct!"); break;
-                case "c": Console.WriteLine("Incorrect."); break;
-                case "d": Console.WriteLine("Incorrect."); break;
-                default: Console.WriteLine("Invalid input."); break;
-            }   
+                for (j = 1; j <= 7 - i; j++) Console.Write("*");
+                Console.WriteLine("\n");
+            }
         }
     }
 }
