ce6cb394a5549914251f1a6161e461b5f30fa74c
Nathan Reinauer <nathanreinauer@gmail.com>
2016-06-12 18:16:48 -0700

    while 2

--- Drillz/Drillz/Program.cs
+++ Drillz/Drillz/Program.cs
@@ -11,26 +11,14 @@ namespace Drillz
     {
         static void Main(string[] args)
         {
-            bool run = true;
-
-            while (run)
+            int i = 1;
+            while (i <= 122)
             {
-                Console.WriteLine("What is the command keyword to exit a loop in C#?");
-                Console.WriteLine("a. int");
-                Console.WriteLine("b. continue");
-                Console.WriteLine("c. break");
-                Console.WriteLine("d. exit");
+                Console.Write((char)i + "\t");
+                if (i % 10 == 0)
+                    Console.WriteLine("\n");
 
-                var answer = Console.ReadLine();
-
-                switch (answer)
-                {
-                    case "a": Console.WriteLine("Incorrect."); break;
-                    case "b": Console.WriteLine("Incorrect."); break;
-                    case "c": Console.WriteLine("Correct!"); run = false; break;
-                    case "d": Console.WriteLine("Incorrect!"); break;
-                    default: Console.WriteLine("Invalid input."); break;
-                }
+                i++;
             }
         }
     }
