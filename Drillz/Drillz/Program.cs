a752fe26c6efba21f126c9311bc01d778658f5d2
Nathan Reinauer <nathanreinauer@gmail.com>
2016-06-12 23:04:45 -0700

    Statistics

--- Drillz/Drillz/Program.cs
+++ Drillz/Drillz/Program.cs
@@ -9,48 +9,82 @@ namespace Drillz
     {
         static void Main(string[] args)
         {
-            int size;
+            int n;
+            float mean, median, std;
+            Console.Write("Enter number of data points:");
+            n = int.Parse(Console.ReadLine());
+            if (n < 3)
+            {
+                Console.WriteLine("The number of data points should be greater than 2.");
+            }
+            else
+            {
+                int[] dataset = new int[n];
 
-            Console.Write("Pascal triangle size:");
+                int i = 0;
+                for (i = 0; i < n; i++)
+                {
+                    Console.Write("[{0}]:", i);
+                    dataset[i] = int.Parse(Console.ReadLine());
+                }
 
-            size = Int32.Parse(Console.ReadLine());
+                bubblesort(dataset, n);
 
-            printPascalTr(size);
+                int sum = 0;
+                int j = 0;
+                while (j < n)
+                {
+                    sum = sum + dataset[j];
+                    j++;
+                }
+                mean = (float)sum / n;
+                if (n % 2 != 0) median = dataset[n / 2];
+                else median = (dataset[(n / 2) - 1] + dataset[n / 2]) / (float)2;
 
+                int[,] mode = new int[n, 2];
+                for (i = 0; i < 2; i++)
+                    for (j = 0; j < n; j++) mode[j, i] = 0;
+                mode[0, 0] = 1;
+
+                for (i = 0; i < n; i++)
+                    for (j = 0; j < n - 1; j++)
+                        if (dataset[i] == dataset[j + 1]) { ++mode[i, 0]; mode[i, 1] = dataset[i]; }
+
+                int max;
+                int k = 0;
+                max = mode[0, 0];
+                for (j = 0; j < n; j++)
+                    if (max < mode[j, 0]) { max = mode[j, 0]; k = j; }
+
+                float temp = 0.0f;
+
+                for (j = 0; j < n; j++)
+                {
+                    temp = temp + (float)Math.Pow(dataset[j] - mean, 2);
+                }
+                std = (float)Math.Sqrt(temp / (n - 1));
+
+                Console.WriteLine("Statistical Information:");
+                Console.WriteLine("Arithmetic mean:{0}", mean);
+                Console.WriteLine("Median:{0}", median);
+                if (mode[k, 1] != 0)
+                    Console.WriteLine("Mode:{0}", mode[k, 1]);
+                else Console.WriteLine("Mode: no mode");
+                Console.WriteLine("Standard deviation:{0}", std);
+            }
             Console.ReadLine();
         }
-        public static void printPascalTr(int size)
+        static void bubblesort(int[] dataset, int n)
         {
-            int[,] PascalTr = new int[size, size];
-            int row, col;
-
-            for (row = 0; row < size; row++)
-                for (col = 0; col < size; col++) PascalTr[row, col] = 0;
-
-            PascalTr[0, 0] = 1;
-
-            PascalTr[1, 0] = 1;
-
-            PascalTr[1, 1] = 1;
-
-            for (row = 2; row < size; row++)
-            {
-                PascalTr[row, 0] = 1;
-
-                for (col = 1; col <= row; col++)
-                {
-                    PascalTr[row, col] = PascalTr[row - 1, col - 1] + PascalTr[row - 1, col];
-                }
-            }
-
-            for (row = 0; row < size; row++)
-            {
-                for (col = 0; col <= row; col++)
-                {
-                    Console.Write("{0}\t", PascalTr[row, col]);
-                }
-                Console.WriteLine();
-            }
+            int i, j;
+            for (i = 0; i < n; i++)
+                for (j = n - 1; j > i; j--)
+                    if (dataset[j] < dataset[j - 1])
+                    {
+                        int temp = dataset[j];
+                        dataset[j] = dataset[j - 1];
+                        dataset[j - 1] = temp;
+                    }
         }
     }
 }
