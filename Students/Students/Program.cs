8231bb2c6abcbfe7c4809fa36209ea741c50606e
Nathan Reinauer <nathanreinauer@gmail.com>
2016-06-13 15:38:18 -0700

    Step 2

--- Students/Students/Program.cs
+++ Students/Students/Program.cs
@@ -11,6 +11,25 @@ namespace Students
         static void Main(string[] args)
         {
         }
+        static void displaymenu()
+        {
+
+            Console.WriteLine("=====================================================");
+
+            Console.WriteLine(" MENU ");
+
+            Console.WriteLine("=====================================================");
+
+            Console.WriteLine(" 1.Add student records");
+            Console.WriteLine(" 2.Delete student records");
+            Console.WriteLine(" 3.Update student records");
+            Console.WriteLine(" 4.View all student records");
+            Console.WriteLine(" 5.Calculate an average of a selected student's scores");
+            Console.WriteLine(" 6.Show student who get the max total score");
+            Console.WriteLine(" 7.Show student who get the min total score");
+            Console.WriteLine(" 8.Find a student by ID");
+            Console.WriteLine(" 9.Sort students by TOTAL");
+        }
     }
     struct student
     {
@@ -24,4 +43,5 @@ namespace Students
         public float final;
         public float total;
     };
+
 }
