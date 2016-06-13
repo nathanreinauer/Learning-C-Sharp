1159146387c3e89b27224d817f0ff1017b074f44
Nathan Reinauer <nathanreinauer@gmail.com>
2016-06-12 18:48:34 -0700

    do while 1

--- Drillz/Drillz/Program.cs
+++ Drillz/Drillz/Program.cs
@@ -11,15 +11,26 @@ namespace Drillz
     {
         static void Main(string[] args)
         {
-            int i = 1;
-            while (i <= 122)
-            {
-                Console.Write((char)i + "\t");
-                if (i % 10 == 0)
-                    Console.WriteLine("\n");
+            string choice;
+            string con = "y";
+            Console.WriteLine("a.quit");
+            Console.WriteLine("b.continue");
+            Console.WriteLine("c.break");
+            Console.WriteLine("d.exit");
 
-                i++;
-            }
+            do
+            {
+                Console.Write("What is the command keyword to exit a loop in C#?");
+                choice = Console.ReadLine();
+
+                if (choice == "c")
+                {
+                    Console.WriteLine("Correct!");
+                }
+                else Console.WriteLine("Incorrect.");
+                Console.Write("Press 'y' to play again.");
+                con = Console.ReadLine().ToString();
+            } while (con == "y");
         }
     }
 }
