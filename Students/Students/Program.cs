336e479c9bc22e1db690dad1c46e8c8134da59b2
Nathan Reinauer <nathanreinauer@gmail.com>
2016-06-13 15:36:12 -0700

    Step 1 of student program

---
+++ Students/Students/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    struct student
+    {
+        public string stnumber;
+        public string stname;
+        public string sex;
+        public float quizz1;
+        public float quizz2;
+        public float assigment;
+        public float midterm;
+        public float final;
+        public float total;
+    };
+}
